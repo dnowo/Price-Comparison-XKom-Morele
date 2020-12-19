cebe53789c18f10b50e2f5f7107934dfea1afd52
Daniel <39437786+dnowo@users.noreply.github.com>
2020-12-19 15:59:27 +0100

    Create scraper from two sites
    Also deleted .idea

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
 import scrapy
+from scrapy.crawler import CrawlerProcess
 
 
-class BlogSpider(scrapy.Spider):
-    name = 'blogspider'
-    start_urls = ['https://blog.scrapinghub.com']
+class ShopsSpider(scrapy.Spider):
+    name = "shop"
+
+    def start_requests(self):
+        urls = [
+            # "https://www.euro.com.pl/",
+            "https://www.morele.net/",
+            "https://www.x-kom.pl/"
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        for title in response.css('.post-header>h2'):
-            yield {'title': title.css('a ::text').get()}
+        page = response.url.split("/")[-2]
+        filename = f'{page}.html'
+        print(response.css('title'))
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')
 
-        for next_page in response.css('a.next-posts-link'):
-            yield response.follow(next_page, self.parse)
 
-# class First_scrapyItem(scrapy.Item):
-#     name = scrapy.Field()
-#     url = scrapy.Field()
-#     desc = scrapy.Field()
-#
-#
-# class firstSpider(scrapy.Spider):
-#     name = "first"
-#     allowed_domains = ["x-kom.pl", "morele.net"]
-#
-#     start_urls = [
-#         "https://www.x-kom.pl/",
-#         "https://www.morele.net/"
-#     ]
-#
-#     def parse(self, response):
-#         filename = response.url.split("/")[-2] + '.html'
-#         with open(filename, 'wb') as f:
-#             f.write(response.body)
+process = CrawlerProcess({
+    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+})
+process.crawl(ShopsSpider)
+process.start()
