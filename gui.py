a56055c9aac49274b80191036379c57ca81b253c
Daniel <39437786+dnowo@users.noreply.github.com>
2020-12-20 15:35:47 +0100

    feature/add morele scraper

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import QWidget, QPushButton, QLabel, QGridLayout, QLineEdit, QDesktopWidget
 from PyQt5.QtCore import pyqtSlot
 
-from shopscrapper import scrapFromXkom
+from shopscrapper import scrapFromXkom, scrapFromMorele
 
 
 class Gui(QWidget):
@@ -11,7 +11,8 @@ class Gui(QWidget):
         self.textboxCompareNameInput = QLineEdit()
         self.labelCompareNameInput = QLabel(self)
 
-        self.labelComparedItem = QLabel(self)
+        self.labelXkomComparedItem = QLabel(self)
+        self.labelMoreleComparedItem = QLabel(self)
 
         self.labelXkomShopName = QLabel(self)
         self.labelXkomItemPrice = QLabel(self)
@@ -21,58 +22,92 @@ class Gui(QWidget):
         self.labelMoreleItemPrice = QLabel(self)
         self.labelMoreleItemAvailability = QLabel(self)
 
+        self.labelError = QLabel(self)
+
+        self.labelDiffrence = QLabel(self)
+
         self.initUI()
 
     def initUI(self):
         grid = QGridLayout()
 
         # Input label, textbox, button
-        self.labelCompareNameInput.setText("Wprowadź nazwę przedmiotu: ")
+        self.labelCompareNameInput.setText("Wprowadź słowa kluczowe przedmiotu: ")
         self.labelCompareNameInput.adjustSize()
-        self.labelComparedItem.adjustSize()
 
-        self.textboxCompareNameInput.resize(100, 20)
+        self.textboxCompareNameInput.adjustSize()
 
         buttonCompareNameInput = QPushButton("Porównaj", self)
         buttonCompareNameInput.clicked.connect(self.runScraping)
 
         grid.addWidget(self.labelCompareNameInput, 1, 1)
-        grid.addWidget(self.textboxCompareNameInput, 1, 2)
-        grid.addWidget(buttonCompareNameInput, 1, 3)
+        grid.addWidget(self.textboxCompareNameInput, 1, 2, 1, 2)
+        grid.addWidget(buttonCompareNameInput, 1, 4)
 
-        grid.addWidget(self.labelComparedItem, 2, 2)
+        self.labelError.adjustSize()
+        grid.addWidget(self.labelError, 2, 2)
+
+        self.labelDiffrence.adjustSize()
+        grid.addWidget(self.labelDiffrence, 7, 2)
 
         # Show diffrence
         self.labelXkomShopName.adjustSize()
         self.labelXkomItemPrice.adjustSize()
         self.labelXkomItemAvailability.adjustSize()
+        self.labelXkomComparedItem.adjustSize()
 
         grid.addWidget(self.labelXkomShopName, 3, 1)
-        grid.addWidget(self.labelXkomItemPrice, 4, 1)
-        grid.addWidget(self.labelXkomItemAvailability, 5, 1)
+        grid.addWidget(self.labelXkomComparedItem, 4, 1)
+        grid.addWidget(self.labelXkomItemPrice, 5, 1)
+        grid.addWidget(self.labelXkomItemAvailability, 6, 1)
 
         self.labelMoreleShopName.adjustSize()
         self.labelMoreleItemPrice.adjustSize()
         self.labelMoreleItemAvailability.adjustSize()
+        self.labelMoreleComparedItem.adjustSize()
 
-        grid.addWidget(self.labelMoreleShopName, 3, 2)
-        grid.addWidget(self.labelMoreleItemPrice, 4, 2)
-        grid.addWidget(self.labelMoreleItemAvailability, 5, 2)
+        grid.addWidget(self.labelMoreleShopName, 3, 3)
+        grid.addWidget(self.labelMoreleComparedItem, 4, 3)
+        grid.addWidget(self.labelMoreleItemPrice, 5, 3)
+        grid.addWidget(self.labelMoreleItemAvailability, 6, 3)
 
         # Show gui
         self.setLayout(grid)
         self.move(QDesktopWidget().availableGeometry().center())
-        self.setWindowTitle("Porównaj ceny")
+        self.setWindowTitle("Porównaj ceny w sklepach")
         self.show()
 
     @pyqtSlot()
     def runScraping(self):
         xKomProduct = scrapFromXkom(self.textboxCompareNameInput.text())
-        # Item name label
-        self.labelComparedItem.setText(str(xKomProduct.product_name))
+        moreleProduct = scrapFromMorele(self.textboxCompareNameInput.text())
+        err = -1
+        errString = ["Nie mogę znaleźć przedmiotu w sklepie: "]
+
+        if xKomProduct == 0:
+            self.labelError.setText(errString[0] + "x-kom.pl")
+            err = 0
+        if moreleProduct == 0:
+            self.labelError.setText(errString[0] + "Morele.net")
+            err = 1
+        if xKomProduct == 0 and moreleProduct == 0:
+            self.labelError.setText(errString[0] + "Morele.net oraz x-kom.pl")
+            err = 2
+        if err != -1:
+            return
+        self.labelError.setText("")
 
         # Item from XKom.pl
+        self.labelXkomComparedItem.setText(str(xKomProduct.product_name))
         self.labelXkomShopName.setText(str(xKomProduct.shop_name))
         self.labelXkomItemPrice.setText(str(xKomProduct.product_price))
         self.labelXkomItemAvailability.setText(
             str("Dostępny" if xKomProduct.product_availability is True else "Niedostępny"))
+
+        # Item from Morele.net
+        self.labelMoreleComparedItem.setText(str(moreleProduct.product_name))
+        self.labelMoreleShopName.setText(str(moreleProduct.shop_name))
+        self.labelMoreleItemPrice.setText(str(moreleProduct.product_price))
+        self.labelMoreleItemAvailability.setText(
+            str("Dostępny" if moreleProduct.product_availability is True else "Niedostępny"))
+        
--- shopscrapper.py
+++ shopscrapper.py
@@ -1,32 +1,74 @@
-from bs4 import BeautifulSoup
+from functools import reduce
+
 import requests
+from bs4 import BeautifulSoup
 from urllib import request
 
 from Item import Item
 
 xKomUrl = "https://www.x-kom.pl/"
+moreleUrl = "https://www.morele.net/"
 
 
-def response(url: str):
+def responseXkom(url: str):
     html = request.urlopen(url).read().decode('utf8')
     soup = BeautifulSoup(html, 'html.parser')
     return soup
 
 
+def responseMorele(url: str):
+    html = requests.get(url)
+    soup = BeautifulSoup(html.text, 'html.parser')
+    return soup
+
+
 def scrapFromXkom(productQuery: str):
     url = xKomUrl + "szukaj?q=" + productQuery.replace(" ", "%20")
-    soup = response(url)
+    soup = responseXkom(url)
     aResults = soup.find_all("a", class_="sc-1h16fat-0 sc-1yu46qn-10 dFAarG", href=True)
     for a in aResults:
         print(a["href"])
     if len(aResults) < 1:
         print("Nie znaleziono przedmiotu!")
-        return None
+        return 0
     productUrl = xKomUrl + aResults[0]["href"]
     print(productUrl)
-    soupProduct = response(productUrl)
+    soupProduct = responseXkom(productUrl)
     productAvailable = False
     if soupProduct.find("span", class_="sc-1hdxfw1-1 cMQxDU") is not None:
         productAvailable = True
-    product = Item("x-kom", soupProduct.find("h1", class_="sc-1x6crnh-5").text, soupProduct.find("div", class_="u7xnnm-4 iVazGO").text, productAvailable)
-    return product
+    return Item("x-kom",
+                soupProduct.find("h1", class_="sc-1x6crnh-5").text if soupProduct.find("h1", class_="sc-1x6crnh-5") is not None else "Brak danych",
+                soupProduct.find("div", class_="u7xnnm-4 iVazGO").text if soupProduct.find("div", class_="u7xnnm-4 iVazGO") is not None else "Brak danych",
+                productAvailable)
+
+
+def scrapFromMorele(productQuery: str):
+    url = moreleUrl + "wyszukiwarka/0/0/,,,,,,,,,,,,/1/?q=" + productQuery.replace(" ", "+")
+    soup = responseMorele(url)
+    aResults = soup.find_all("a", class_="productLink", href=True)
+    for a in aResults:
+        print(a["href"])
+    if len(aResults) < 1:
+        print("Nie znaleziono przedmiotu!")
+        return 0
+
+    productUrl = moreleUrl + str(aResults[0]["href"]).replace("/", "", 1)
+    print(productUrl)
+
+    soupProduct = responseMorele(productUrl)
+    productAvailable = True
+    if soupProduct.find("div", class_="prod-available-items") is None:
+        productAvailableString = "0"
+    else:
+        productAvailableString = soupProduct.find("div", class_="prod-available-items").text
+
+    for r in (("Dostępnych", ""), ("szt.", ""), (" ", ""), ("Zostało", ""), ("tylko", "")):
+        productAvailableString = productAvailableString.replace(*r)
+    if productAvailableString is None or int(productAvailableString) < 1:
+        productAvailable = False
+    return Item("Morele.net",
+                soupProduct.find("h1", class_="prod-name").text if soupProduct.find("h1", class_="prod-name") is not None else "Brak danych",
+                soupProduct.find_all("div", class_="product-price")[0]["data-default"] if soupProduct.find_all("div", class_="product-price")[0]["data-default"] is not None else "Brak danych",
+                productAvailable)
+
